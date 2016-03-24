4b7fd52bee26a541e89e22cb270ca003b5a10172
Kornilov <ivanmanul@gmail.com>
2016-03-24 10:39:01 +0500

    Initial commit

---
+++ src/lesson01/Main.java
@@ -0,0 +1,24 @@
+package lesson01;
+
+/**
+ * User: gkislin
+ * Date: 18.06.2014
+ */
+public class Main {
+    /**
+     * /**
+     * First java program
+     *
+     * @param args : program arguments
+     */
+    public static void main(String[] args) {
+        System.out.format("Hello %s!\n", args[0]);
+      //  Car raceCar = new RaceCar();
+     //   Car raceCar2 = new RaceCar();
+     //   Car simpleCar = new SimpleCar();
+     //   raceCar2.speed=200;
+     //   System.out.println(raceCar.getSpeed());
+    //    raceCar.getDescription();
+     //   simpleCar.getDescription();
+    }
+}
---
+++ src/lesson01/Resume.java
@@ -0,0 +1,12 @@
+package lesson01;
+
+/**
+ * Kornilov
+ * 22.03.2016
+ */
+public interface Resume {
+
+    String getResume ();
+    void setResume (String fullName);
+    double  getEngineVolume ();
+}
