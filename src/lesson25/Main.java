31ca154709551f5c112ee77a2c134b151d7f8e2c
Kornilov <ivanmanul@gmail.com>
2016-03-25 10:14:48 +0500

    class implementation

---
+++ src/lesson25/Main.java
@@ -0,0 +1,13 @@
+package lesson25;
+
+import java.util.Date;
+
+/**
+ * Kornilov
+ * 25.03.2016
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
