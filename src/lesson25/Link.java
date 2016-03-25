3a8ca18ae5aa707130ba7f48794e1f199c2fdbb6
Kornilov <ivanmanul@gmail.com>
2016-03-25 10:23:41 +0500

    class implementation

---
+++ src/lesson25/Link.java
@@ -0,0 +1,10 @@
+package lesson25;
+
+/**
+ * Kornilov
+ * 25.03.2016
+ */
+public class Link {
+    private String name;
+    private String url;
+}
---
+++ src/lesson25/Resume.java
@@ -0,0 +1,11 @@
+package lesson25;
+
+/**
+ * Kornilov
+ * 25.03.2016
+ */
+public class Resume {
+    private String fullName;
+    private String location;
+
+}
